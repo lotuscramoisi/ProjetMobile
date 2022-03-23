e0e73736b84f56ec15865709dc255e5287dd134a
lotuscramoisi <63124539+lotuscramoisi@users.noreply.github.com>
2022-03-23 16:56:11 +0100

    Create fonctionUser.php

---
+++ fonctionUser.php
@@ -0,0 +1,37 @@
+<?php
+    function getUserIP(){
+        if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+            //IP from internet
+            $address=$_SERVER['HTTP_CLIENT_IP'];
+        }
+        elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+            //IP from proxy
+            $address=$_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+        else{
+            $address=$_SERVER['REMOTE_ADDR'];
+        }
+        return $address;
+    }
+
+    function getUserPort(){
+        return $_SERVER['REMOTE_PORT'];
+    }
+
+    function getBrowser(){
+        return getBrowser();
+    }
+
+    function get_operating_system() {
+        $u_agent = $_SERVER['HTTP_USER_AGENT'];
+        $operating_system = 'Unknown Operating System';
+
+        return $operating_system;
+    }
+    
+    echo '</br>User IP Address is'.getUserIP();
+    echo '</br>User Port:'.getUserPort();
+    echo '</br>User Port:'.getBrowser();
+    echo '</br>Navigateur par défaut:'.get_operating_system();
+?>
+
