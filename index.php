9b08aa2b0b91f67ba2b9d5d0458ab4a42fe1d19d
ddruart19 <70936236+ddruart19@users.noreply.github.com>
2022-03-23 12:02:35 +0100

    form

--- index.php
+++ index.php
@@ -25,6 +25,12 @@
     <div class="modal fade" id="signIn" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Sign In</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
                 <form>
                     <div class="form-group">
                         <label for="exampleInputEmail1">Email address</label>
