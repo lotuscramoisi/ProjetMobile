4944559ff87f3144eb2c60c32bdc585b3234ee9d
ddruart19 <70936236+ddruart19@users.noreply.github.com>
2022-03-23 12:26:07 +0100

    css

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
         Register
     </button>
     <!-- Modal -->
-    <div class="modal fade" id="signIn" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal" id="signIn" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
