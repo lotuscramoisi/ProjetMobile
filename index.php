29ad57a76c6d7009d8ef079aa1f6625cd71d642b
ddruart19 <dorian.druart@gmail.com>
2022-03-23 17:16:45 +0100

    session

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 
 <head>
     <?php
+        // Inclusion des fichiers de fonctions
         include 'functions.php'; 
         include 'connexiondb.php'; 
+        // Connexion à la BDD en tant qu'admin
         $conn=connectDBasAdmin();
+        // Démarrage de la session pour créer les variables $_SESSION
+        session_start ();
     ?>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
