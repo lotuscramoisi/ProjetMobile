2e17917e45786d80d7b2e6034dc383febd3c8ad5
ddruart19 <70936236+ddruart19@users.noreply.github.com>
2022-03-23 12:29:29 +0100

    csss

--- index.php
+++ index.php
@@ -23,12 +23,12 @@
         Register
     </button>
     <!-- Modal -->
-    <div class="modal" id="signIn" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="signIn" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">Sign In</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <form>
                     <div class="modal-body">
