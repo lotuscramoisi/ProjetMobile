54b79ddf6fa860eaa5b35008c6608c0b8ac6eb89
ddruart19 <70936236+ddruart19@users.noreply.github.com>
2022-03-23 12:30:25 +0100

    cssss

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">Sign In</h5>
-                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <form>
                     <div class="modal-body">
