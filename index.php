273c3133d47c0e042843f771ee3f87985f8186d9
ddruart19 <70936236+ddruart19@users.noreply.github.com>
2022-03-23 11:58:20 +0100

    form

--- index.php
+++ index.php
@@ -15,12 +15,14 @@
 
 <body>
     <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-        Launch demo modal
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#signIn">
+        Sign in
     </button>
-
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+        Register
+    </button>
     <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="signIn" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <form>
