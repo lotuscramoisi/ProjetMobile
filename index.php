53ed6ebd7e19d6687cf5742f3d098a1badbcfdc5
ddruart19 <dorian.druart@gmail.com>
2022-03-23 15:16:28 +0100

    oups

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <?php
         include 'functions.php'; 
-        $conn=connectDB("localhost","stage2019", "root", "");
+        // $conn=connectDB("localhost","stage2019", "root", "");
     ?>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
