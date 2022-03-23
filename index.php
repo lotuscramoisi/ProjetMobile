621823e26cc083ac1e8f552f7c667f012a1fe6cd
ddruart19 <dorian.druart@gmail.com>
2022-03-23 18:40:59 +0100

    push

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
 
                         <div class="form-group">
                             <label for="exampleInputEmail1">Email address/Username</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="login">
+                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email or username" name="login">
                             <small id="emailHelp" class="form-text text-muted">
                                 Not registered yet ?
                                 <button type="button" class="btn btn-link btn-sm" data-toggle="modal" data-dismiss="modal" data-target="#register" tabindex="-1">
@@ -107,11 +107,13 @@
                             <label for="exampleInputPassword1">Password confirmation</label>
                             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password confirmation" name="passwordverif">
                         </div>
+                        <small id="passwordnotsame" class="form-text text-muted">
+                        </small>
                     </div>
                     <div class="modal-footer">
                         <small id="emailHelp" class="form-text text-muted">
                             Already registered ?
-                            <button type="button" class="btn btn-link btn-sm" data-toggle="modal" data-dismiss="modal" data-target="#signIn">
+                            <button type="button" class="btn btn-link btn-sm" data-toggle="modal" data-dismiss="modal" data-target="#signIn" tabindex="-1">
                                 Login
                             </button>
                         </small>
