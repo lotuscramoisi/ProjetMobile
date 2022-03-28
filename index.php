a18d534998601698dc0b0f8603d50fdb5d6ae379
ddruart19 <dorian.druart@gmail.com>
2022-03-28 22:09:39 +0200

    grid

--- index.php
+++ index.php
@@ -199,12 +199,31 @@
     </div>
     <!-- START Liste des informations -->
 
-    <div class="row">
-        <div class="col">col</div>
-        <div class="col">col</div>
-        <div class="w-100"></div>
-        <div class="col">col</div>
-        <div class="col">col</div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                Adresse IP
+            </div>
+            <div class="col">
+                192.168.1.1
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                Navigateur
+            </div>
+            <div class="col">
+                Google Chrome
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                Appareil
+            </div>
+            <div class="col">
+                Samsung 11
+            </div>
+        </div>
     </div>
 
     <!-- END Liste des informations -->
