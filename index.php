62cc777286ffe881a274e7c1aee50dc7875adb95
ddruart19 <dorian.druart@gmail.com>
2022-03-30 21:25:25 +0200

    dqs

--- index.php
+++ index.php
@@ -80,6 +80,19 @@
         }
         // FIN : COTE Client
     </script>
+    <!-- DEBUT : COTE Serveur -->
+    <?php
+    if (isset($_GET["error"])) {
+        $errormsg = $_GET["error"];
+        echo "$('#alertMessageRegister').empty();";
+        echo "<script>$('#register').modal('show');"; //Affichage du formulaire d'enregistrement
+        //Si l'adresse email existe déjà dans notre DB
+        if ($errormsg  == "existingmail") {
+            echo "$('#alertMessageRegister').append('<font color=red>Email address already taken</font>');</script>";
+        }
+    }
+    ?>
+    <!-- FIN : COTE Serveur -->
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
@@ -94,19 +107,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>What's my info ?!</title>
 </head>
-<!-- DEBUT : COTE Serveur -->
-<?php
-if (isset($_GET["error"])) {
-    $errormsg = $_GET["error"];
-    echo "$('#alertMessageRegister').empty();";
-    echo "<script>$('#register').modal('show');"; //Affichage du formulaire d'enregistrement
-    //Si l'adresse email existe déjà dans notre DB
-    if ($errormsg  == "existingmail") {
-        echo "$('#alertMessageRegister').append('<font color=red>Email address already taken</font>');</script>";
-    }
-}
-?>
-<!-- FIN : COTE Serveur -->
+
 
 <body>
     <!-- Nav Bar -->
