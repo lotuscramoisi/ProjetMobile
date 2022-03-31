501424bc8152f561ac35059425c875a949ba953b
ddruart19 <dorian.druart@gmail.com>
2022-03-31 18:10:33 +0200

    dsf

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
 
 <head>
     <!-- DEBUT IMPORT -->
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="cookiealert.css">
     <link rel="stylesheet" href="style.css">
+
     <!-- FIN IMPORT -->
     <?php
     // Inclusion des fichiers de fonctions
@@ -307,14 +308,6 @@
     <ul class="list-group">
         <li class="list-group-item d-flex justify-content-between align-items-center">
             Adresse IP
-            <span class="badge badge-primary badge-pill"><?php echo getUserIP() ?></span>
-        </li>
-		<li class="list-group-item d-flex justify-content-between align-items-center">
-            IPV4
-            <span class="badge badge-primary badge-pill"><?php echo getUserIP() ?></span>
-        </li>
-		<li class="list-group-item d-flex justify-content-between align-items-center">
-            Proxy
             <span class="badge badge-primary badge-pill"><?php echo getUserIP() ?></span>
         </li>
         <li class="list-group-item d-flex justify-content-between align-items-center">
