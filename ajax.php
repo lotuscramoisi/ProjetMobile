a69342ac69f787c057554afaa3d1f9676849d53c
lotuscramoisi <filoodu412@gmail.com>
2022-04-02 14:45:40 +0200

    trezsqt

--- ajax.php
+++ ajax.php
@@ -1,9 +1,9 @@
 <?php  
 
-    $userIp = $_GET["fname"];
+    $userIp = $_GET["userIp"];
 
 
-    echo "Hi there! Welcome to our website.";
+    echo "Hi there! Welcome to our website."+$userIp;
 
     // <script>
     //     $.getJSON("https://ipgeolocation.abstractapi.com/v1/?api_key=8aa482a8a0d3496c83f1e6a6c748f79b&ip_address=166.171.248.255", function(data) {
