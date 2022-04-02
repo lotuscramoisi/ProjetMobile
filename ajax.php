48ccbd459ea784192a700a49017233b2c86ad0d3
lotuscramoisi <filoodu412@gmail.com>
2022-04-02 14:46:07 +0200

    conca

--- ajax.php
+++ ajax.php
@@ -3,7 +3,7 @@
     $userIp = $_GET["userIp"];
 
 
-    echo "Hi there! Welcome to our website."+$userIp;
+    echo "Hi there! Welcome to our website.".$userIp;
 
     // <script>
     //     $.getJSON("https://ipgeolocation.abstractapi.com/v1/?api_key=8aa482a8a0d3496c83f1e6a6c748f79b&ip_address=166.171.248.255", function(data) {
