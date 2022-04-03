4a9df7a0a9019d9b52cd680c297dce59fbcba5b5
lotuscramoisi <filoodu412@gmail.com>
2022-04-03 15:29:23 +0200

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -4,7 +4,6 @@
     
     if ($registration == "success"){
         include 'fonctionUser.php';
-        include 'projet.env';
         $userIp = getUserIP(); 
         //$url = "https://ipgeolocation.abstractapi.com/v1/?api_key=8aa482a8a0d3496c83f1e6a6c748f79b";
 
