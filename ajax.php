0a7dc4a7814aed5b22903b980e5d6252a1278f82
lotuscramoisi <filoodu412@gmail.com>
2022-04-03 12:19:09 +0200

    corr

--- ajax.php
+++ ajax.php
@@ -5,7 +5,6 @@
         include 'fonctionUser.php';
         $userIp = getUserIP();
         $url = "https://ipgeolocation.abstractapi.com/v1/?api_key=8aa482a8a0d3496c83f1e6a6c748f79b&ip_address=".$userIp;
-        $json = file_get_contents($url);
 
         // Initialize cURL.
         $ch = curl_init();
