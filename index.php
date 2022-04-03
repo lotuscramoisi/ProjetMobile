1b32f661c67411b62f7e89d8e1f142fa325cf513
lotuscramoisi <filoodu412@gmail.com>
2022-04-03 15:37:36 +0200

    erreur

--- index.php
+++ index.php
@@ -145,7 +145,7 @@
 
         function create () {
         $.ajax({
-            url:"index.js",    //the page containing php script
+            url:"inde.js",    //the page containing php script
             type: "post",    //request type,
             dataType: 'json',
             data: {registration: "success"},
