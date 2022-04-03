f49f2ffaf5370741783b011b95e551cdd6e81f28
lotuscramoisi <filoodu412@gmail.com>
2022-04-03 12:18:26 +0200

    even better with curl instead of json_encode

--- ajax.php
+++ ajax.php
@@ -7,7 +7,26 @@
         $url = "https://ipgeolocation.abstractapi.com/v1/?api_key=8aa482a8a0d3496c83f1e6a6c748f79b&ip_address=".$userIp;
         $json = file_get_contents($url);
 
-        echo json_encode(array("abc"=>'successfuly registered'.$json));
+        // Initialize cURL.
+        $ch = curl_init();
+
+        // Set the URL that you want to GET by using the CURLOPT_URL option.
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        // Set CURLOPT_RETURNTRANSFER so that the content is returned as a variable.
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        // Set CURLOPT_FOLLOWLOCATION to true to follow redirects.
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+        // Execute the request.
+        $data = curl_exec($ch);
+
+        // Close the cURL handle.
+        curl_close($ch);
+
+        // Print the data out onto the page.
+        echo $data;
     }
 
 ?>
