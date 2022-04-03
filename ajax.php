411354cd60ca657721a34364f6d6c6e47de32ac9
lotuscramoisi <filoodu412@gmail.com>
2022-04-03 12:02:28 +0200

    <script>

--- ajax.php
+++ ajax.php
@@ -4,14 +4,14 @@
     if ($registration == "success"){
         include 'fonctionUser.php';
         $userIp = getUserIP();
-        //getUserIP() 
+        <script>
+            $.getJSON("https://ipgeolocation.abstractapi.com/v1/?api_key=8aa482a8a0d3496c83f1e6a6c748f79b&ip_address=166.171.248.255", function(data) {
+                 console.log(data.ip_address);
+                 console.logget(data.country);
+             })
+                
+        </script>
         echo json_encode(array("abc"=>'successfuly registered'.$userIp));
     }
-    // <script>
-    //     $.getJSON("https://ipgeolocation.abstractapi.com/v1/?api_key=8aa482a8a0d3496c83f1e6a6c748f79b&ip_address=166.171.248.255", function(data) {
-    //         console.log(data.ip_address);
-    //         console.logget(data.country);
-    //     })
-            
-    // </script>
+
 ?>
