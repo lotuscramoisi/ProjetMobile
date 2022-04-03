1340867c69e92027317046b9f92f8522268e9ea8
lotuscramoisi <filoodu412@gmail.com>
2022-04-03 16:00:33 +0200

    ;

--- ajax.php
+++ ajax.php
@@ -2,7 +2,7 @@
     $registration = $_POST['registration'];
 
     if ($registration == "success"){
-        //require_once realpath(__DIR__ . '/vendor/autoload.php')
+        require_once realpath(__DIR__ . '/vendor/autoload.php');
         // Looing for .env at the root directory
         // $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
         // $dotenv->load();
