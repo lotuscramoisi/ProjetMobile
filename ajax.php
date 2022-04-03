fe28adc862f9293b5cb4071f20ace0c83e2ab3aa
lotuscramoisi <filoodu412@gmail.com>
2022-04-03 11:54:49 +0200

    json encode

--- ajax.php
+++ ajax.php
@@ -2,7 +2,7 @@
     include 'fonctionUser.php';
     $userIp = $_GET["userIp"];
     //getUserIP() 
-    //echo "Hi there! Welcome to our website.".$userIp;
+    echo json_encode("Hi there! Welcome to our website.".$userIp);
 
     // <script>
     //     $.getJSON("https://ipgeolocation.abstractapi.com/v1/?api_key=8aa482a8a0d3496c83f1e6a6c748f79b&ip_address=166.171.248.255", function(data) {
