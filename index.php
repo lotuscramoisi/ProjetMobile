8d55df69939f83a05dc7ea24024b4158f3f47c69
lotuscramoisi <63124539+lotuscramoisi@users.noreply.github.com>
2022-04-04 11:55:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -378,7 +378,7 @@
             <span class="badge badge-primary badge-pill" id="Ville"></span>
         </li>
         <li class="list-group-item d-flex justify-content-between align-items-center">
-            Ville
+            Géolocalisation
             <span class="badge badge-primary badge-pill" id="location"></span>
         </li>
 
