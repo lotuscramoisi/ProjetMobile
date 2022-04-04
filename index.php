bd92a3d3d10d14bbc2a61fb2740d6509c26be096
lotuscramoisi <63124539+lotuscramoisi@users.noreply.github.com>
2022-04-04 11:50:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -25,6 +25,12 @@
     <script>
         // DEBUT AJAX FORMULAIRE REGISTER
         $(document).ready(function() {
+            //Géolocalisation
+            if(navigator.geolocation){
+                navigator.geolocation.getCurrentPosition(showLocation);
+            }else{ 
+                $('#location').html('Geolocation is not supported by this browser.');
+            }
             $("#registerform").submit(function(event) {
                 //Empêche le formulaire d'être envoyé de manière classique
                 event.preventDefault();
@@ -156,6 +162,23 @@
                 document.getElementById("Region").innerHTML = ecrit.city;
             }
         });
+
+        function showLocation(position){
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+            $.ajax({
+                type:'POST',
+                url:'getLocation.php',
+                data:'latitude='+latitude+'&longitude='+longitude,
+                success:function(msg){
+                    if(msg){
+                    $("#location").html(msg);
+                    }else{
+                        $("#location").html('Not Available');
+                    }
+                }
+            });
+        }
     }
 
     </script>
