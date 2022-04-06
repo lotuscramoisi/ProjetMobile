3d12239ec204df11b37e8b58ec2025f4e02c3bf3
lotuscramoisi <filoodu412@gmail.com>
2022-04-06 15:49:39 +0200

    Update getLocation.php

--- getLocation.php
+++ getLocation.php
@@ -3,16 +3,25 @@
 if(!empty($_POST['latitude']) && !empty($_POST['longitude'])){
     //send request and receive json data by latitude and longitude
     $url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='.trim($_POST['latitude']).','.trim($_POST['longitude']).'&sensor=false';
-    $json = @file_get_contents($url);
-    console.log($json);
-    $data = json_encode($json);
-    console.log($json);
-    //if request status is successful
-    if($status == "OK"){
-        //return Json to ajax
-        echo $json;
-    }else{
-        console.log('status not ok for getLocation()');
-    }
+    // Initialize cURL.
+    $ch = curl_init();
+
+    // Set the URL that you want to GET by using the CURLOPT_URL option.
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    // Set CURLOPT_RETURNTRANSFER so that the content is returned as a variable.
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Set CURLOPT_FOLLOWLOCATION to true to follow redirects.
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+    // Execute the request.
+    $data = curl_exec($ch);
+
+    // Close the cURL handle.
+    curl_close($ch);
+    //test
+    // Print the data out onto the page.
+    echo json_encode($data);
 }
 ?>
