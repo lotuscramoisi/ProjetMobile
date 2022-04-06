34c847ab079aeb3d8275a524d6911fdabfa9c0f0
lotuscramoisi <filoodu412@gmail.com>
2022-04-06 15:46:31 +0200

    Update getLocation.php

--- getLocation.php
+++ getLocation.php
@@ -4,7 +4,9 @@ if(!empty($_POST['latitude']) && !empty($_POST['longitude'])){
     //send request and receive json data by latitude and longitude
     $url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng='.trim($_POST['latitude']).','.trim($_POST['longitude']).'&sensor=false';
     $json = @file_get_contents($url);
-    
+    console.log($json);
+    $data = json_encode($json);
+    console.log($json);
     //if request status is successful
     if($status == "OK"){
         //return Json to ajax
