465bcac5157ea65b469fdbfee8aa68e61ad3f146
lotuscramoisi <filoodu412@gmail.com>
2022-04-06 16:58:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -183,6 +183,8 @@
 
         function showPosition(position) {
             var x = document.getElementById("location");
+
+            x.setAttribute("src","https://www.google.com/maps/embed/v1/place?key=<?php echo $APIGOO?>&q="position.coords.latitude"");
             // x.innerHTML = "Latitude: " + position.coords.latitude +
             //     "<br>Longitude: " + position.coords.longitude;
 
