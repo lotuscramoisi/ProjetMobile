96feaf8f6b3707e09735bfa5e204694e4837f057
lotuscramoisi <filoodu412@gmail.com>
2022-04-06 16:36:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -362,8 +362,18 @@
             Géolocalisation
             <span class="badge badge-primary badge-pill" id="location"></span>
         </li>
-
     </ul>
+
+    <iframe
+        width="600"
+        height="450"
+        style="border:0"
+        loading="lazy"
+        allowfullscreen
+        referrerpolicy="no-referrer-when-downgrade"
+        src="https://www.google.com/maps/embed/v1/place?key=API_KEY
+            &q=Space+Needle,Seattle+WA">
+    </iframe>
 
     <button type="button" onclick="create()">Call API</button>
     <button type="button" onclick="getLocation()">Geolocalisation</button>
