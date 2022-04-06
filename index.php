880790092d8dc5779bdcbde8f64f2bbb65087f83
lotuscramoisi <filoodu412@gmail.com>
2022-04-06 16:25:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,16 +3,14 @@
 
 <head>
     <?php
-        // Inclusion des fichiers de fonctions
-        include 'functions.php';
-        include 'connexiondb.php';
-        include 'fonctionUser.php';
-        // Connexion à la BDD en tant qu'admin
-        $conn = connectDBasAdmin();
-        // Démarrage de la session pour créer les variables $_SESSION
-        session_start();
-
-        include 'fonctionUser.php';
+    // Inclusion des fichiers de fonctions
+    include 'functions.php';
+    include 'connexiondb.php';
+    include 'fonctionUser.php';
+    // Connexion à la BDD en tant qu'admin
+    $conn = connectDBasAdmin();
+    // Démarrage de la session pour créer les variables $_SESSION
+    session_start();
     ?>
     <!-- DEBUT IMPORT -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -192,7 +190,7 @@
                     latitude : position.coords.latitude
                 },
                 success: function(result) {
-                    x.innerHTML = "Vide";
+                    x.innerHTML = result;
                 },
                 error: function(xhr, textStatus, errorThrown){
                     alert('request failed->'+textStatus);
