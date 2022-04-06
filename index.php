5511d3244c073a44fd16c467f0e8ef4382c07ca0
lotuscramoisi <filoodu412@gmail.com>
2022-04-06 17:00:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -170,8 +170,6 @@
 
 
         //géolocalisation
-
-
         function getLocation() {
             var x = document.getElementById("location");
             if (navigator.geolocation) {
@@ -184,9 +182,9 @@
         function showPosition(position) {
             var x = document.getElementById("location");
 
-            x.setAttribute("src","https://www.google.com/maps/embed/v1/place?key=<?php echo $APIGOO?>&q="position.coords.latitude);
-            // x.innerHTML = "Latitude: " + position.coords.latitude +
-            //     "<br>Longitude: " + position.coords.longitude;
+            //x.setAttribute("src","https://www.google.com/maps/embed/v1/place?key=<?php echo $APIGOO?>&q="position.coords.latitude);
+            x.innerHTML = "Latitude: " + position.coords.latitude +
+                 "<br>Longitude: " + position.coords.longitude;
 
             // $.ajax({
             //     url: "getLocation.php", //the page containing php script
