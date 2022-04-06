b6d31d134f7462293e5d8c78fad2b941ea92ac51
lotuscramoisi <filoodu412@gmail.com>
2022-04-06 16:28:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 
         require_once realpath(__DIR__ . '/vendor/autoload.php');
         $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
     ?>
     <!-- DEBUT IMPORT -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
