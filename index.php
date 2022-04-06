8144cb918a14a23eeccc515fb814238824b6aa33
lotuscramoisi <filoodu412@gmail.com>
2022-04-06 16:30:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
         $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
         $dotenv->load();
         $userName = $_ENV['PRIVATE_KEY'];
+        echo $userName;
     ?>
     <!-- DEBUT IMPORT -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
