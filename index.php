548e1c35d1ce1ca216694ce9e6148238f98146dd
lotuscramoisi <filoodu412@gmail.com>
2022-04-10 14:55:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -326,7 +326,7 @@
         </div>
     </div>
     <!-- START Liste des informations -->
-    <div class="container-xxl">
+    <div class="container-fluid">
         <div class="row align-items-center">
         <div class="col">
             <ul class="list-group">
