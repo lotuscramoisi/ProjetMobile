10ac89e56d38ad16e50c32b4ea9ac2f6fe968991
LionThibaut <thibaut.lion@gmail.com>
2022-04-20 15:46:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -109,7 +109,7 @@
 						document.getElementById("Continent").innerHTML = ecrit.continent;
                         document.getElementById("Pays").innerHTML = ecrit.country;
 						var flag = ecrit.flag.png;
-						document.getElementById("Drapeau").innerHTML = "<img height='20' width='20' src="+flag+">"
+						document.getElementById("Drapeau").innerHTML = "<img height='30' width='40' src="+flag+">"
                         document.getElementById("Ville").innerHTML = ecrit.region;
                         document.getElementById("Region").innerHTML = ecrit.city;
 						document.getElementById("CodePostal").innerHTML = ecrit.postal_code;
