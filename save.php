985cd4680eda0ec03d71b87745e7c42d02cd51dc
lotuscramoisi <filoodu412@gmail.com>
2022-04-20 18:41:28 +0200

    Update save.php

--- save.php
+++ save.php
@@ -3,21 +3,7 @@ include 'connexiondb.php';
 $conn = connectDBasAdmin();
 
 try {
-    // Récupération des informations du formulaire + vérification de leur intégrité via test_input
-    $username = test_input($_POST["username"]);
-    $date = ;
-  
-    // Requête sql d'insertion des données de l'inscription
-    $sql = "INSERT INTO USER(USERNAME,SIGNUPDATE) VALUES (:username, date('Y-m-d h:i:s'))";
 
-    // Préparation de la requête
-    $stmt = $conn->prepare($sql);
-
-    // Attribution des paramètres de la requête préparée
-    $stmt->bindParam(':username', $username, PDO::PARAM_STR, 25);
-
-    // Exécution de la requête
-    $stmt->execute();
     echo 'Réussi';
 
 } catch (PDOException $e) {
