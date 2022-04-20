74b46a173d8ec1ca775a7de8d7489f2a31a0a482
LionThibaut <thibaut.lion@gmail.com>
2022-04-20 14:50:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -108,7 +108,7 @@
                         document.getElementById("Pays").innerHTML = ecrit.country;
                         document.getElementById("Ville").innerHTML = ecrit.region;
                         document.getElementById("Region").innerHTML = ecrit.city;
-						document.getElementById("Devise").innerHTML = ecrit.currency > currency_name;
+						document.getElementById("Devise").innerHTML = ecrit.currency_name;
                         showPositionLL(ecrit.latitude, ecrit.longitude)
                         }   
                     });
