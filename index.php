29427f162a13d52eff5c28a5488cef57eabb837b
ddruart19 <dorian.druart@gmail.com>
2022-04-20 22:07:59 +0200

    toeast

--- index.php
+++ index.php
@@ -148,22 +148,22 @@
 
             //DEBUT : ENREGISTREMENT DES DONNEES DES USERS DANS LA DB
             //Si l'utilisateur est connecté
-            <?php
-            if (isset($_SESSION['login'])) {
+            // <?php
+            // if (isset($_SESSION['login'])) {
 
-                echo "$.ajax({
-                    url: \"saveinfo.php\", //the page containing php script
-                    type: \"post\", //request type,
-                    dataType: 'json',
-                    data: {
-                        username:  \"" . $_SESSION['login'] .
-                    "\"},
-                    success: function(result) {
+            //     echo "$.ajax({
+            //         url: \"saveinfo.php\", //the page containing php script
+            //         type: \"post\", //request type,
+            //         dataType: 'json',
+            //         data: {
+            //             username:  \"" . $_SESSION['login'] .
+            //         "\"},
+            //         success: function(result) {
                         
-                        }   
-                    });";
-            }
-            ?>
+            //             }   
+            //         });";
+            // }
+            // ?>
             //FIN   : ENREGISTREMENT DES DONNEES DES USERS DANS LA DB
 
         });
