82338c5fc80e693b265f6d93fb6e27c10e5610b6
lotuscramoisi <filoodu412@gmail.com>
2022-04-25 18:34:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -101,6 +101,27 @@
                         // Reenable the inputs
                         $inputs.prop("disabled", false);
                     });
+
+                    $.ajax({
+                    url: "ajax.php", //the page containing php script
+                    type: "post", //request type,
+                    dataType: 'json',
+                    data: {
+                        registration: "success"
+                    },
+                    success: function(result) {
+                        var ecrit = JSON.parse(result);
+                        document.getElementById("Continent").innerHTML = ecrit.continent;
+                        document.getElementById("Pays").innerHTML = ecrit.country;
+                        var flag = ecrit.flag.png;
+                        document.getElementById("Drapeau").innerHTML = "<img height='30px' width='40px' src=" + flag + ">";
+                        document.getElementById("Ville").innerHTML = ecrit.region;
+                        document.getElementById("Region").innerHTML = ecrit.city;
+                        document.getElementById("CodePostal").innerHTML = ecrit.postal_code;
+                        document.getElementById("Devise").innerHTML = ecrit.currency.currency_name;
+                        showPositionLL(ecrit.latitude, ecrit.longitude)
+                    }
+                });
             });
             // FIN AJAX FORMULAIRE LOGIN
 
@@ -463,12 +484,12 @@
                     <label class="form-check-label" for="flexSwitchCheckDefault">Geolocation</label>
                 </div>
 
-                <div class="form-check form-switch">
+                <!-- <div class="form-check form-switch">
                     <input class="form-check-input" type="button" role="switch" id="CallApi" onchange='handleChangeAPI(this);'>
                     <label class="form-check-label" for="flexCheckDisabled">
                     Call API
                     </label>
-                </div>
+                </div> -->
                                 
             </div>
             <div class="col">
