74f7d002ef67c3cefc92617ada40a781480fd951
lotuscramoisi <filoodu412@gmail.com>
2022-04-25 18:51:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -413,7 +413,7 @@
     </div>
 
     <!-- Modal Autorisation -->
-    <div class="modal fade" id="signIn" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="autorisation" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
