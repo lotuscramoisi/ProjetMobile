c13a82d28dbfaee71572ce762844497272a1ea2a
lotuscramoisi <63124539+lotuscramoisi@users.noreply.github.com>
2022-04-26 10:23:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -420,7 +420,7 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title ml-5">Autorisation</h5>
+                        <h5 class="modal-title">Autorisation</h5>
                         <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
