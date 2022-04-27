3318bcbd7b49a45e0f79f0e381bc571a0aa7a554
lotuscramoisi <filoodu412@gmail.com>
2022-04-27 17:32:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -504,29 +504,7 @@
         <div class="row h-100 align-items-center">
             <div class="col-12">
                 <ul class="list-group-flush mt-5">
-                    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="..." class="d-block w-100" alt="...">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="..." class="d-block w-100" alt="...">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="..." class="d-block w-100" alt="...">
-                            </div>
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
-                            data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
-                            data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
-                    </div>
+
                     <li class="list-group-item d-flex justify-content-between align-items-center">
                         Adresse IP
                         <span class="badge badge-primary badge-pill"><?php echo getUserIP() ?></span>
