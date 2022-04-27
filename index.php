139dbcc78c3a6c10235967f6dd605bd646d6cb44
lotuscramoisi <63124539+lotuscramoisi@users.noreply.github.com>
2022-04-27 11:44:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -149,8 +149,6 @@
 						document.getElementById("TypeConnexion").innerHTML = ecrit.connection.connection_type;
 						document.getElementById("FAI").innerHTML = ecrit.connection.isp_name;
 						document.getElementById("OrganisationFAI").innerHTML = ecrit.connection.organization_name;
-						document.getElementById("NomFuseau").innerHTML = ecrit.timezone.name;
-						document.getElementById("HeureConn").innerHTML = ecrit.timezone.current_time;
                         showPositionLL(ecrit.latitude, ecrit.longitude)
                     }
                 });
@@ -531,14 +529,6 @@
 					<li class="list-group-item d-flex justify-content-between align-items-center">
                         Organisation du FAI
                         <span class="badge badge-primary badge-pill" id="OrganisationFAI"></span>
-                    </li>
-					<li class="list-group-item d-flex justify-content-between align-items-center">
-                        Nom du fuseau horaire
-                        <span class="badge badge-primary badge-pill" id="NomFuseau"></span>
-                    </li>
-					<li class="list-group-item d-flex justify-content-between align-items-center">
-                        Heure de connexion
-                        <span class="badge badge-primary badge-pill" id="HeureConn"></span>
                     </li>
                 </ul>                
                                 
