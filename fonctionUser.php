369fa7c95a5192ab10eaff5ab45f5eda9bd2b38a
lotuscramoisi <filoodu412@gmail.com>
2022-04-27 23:57:15 +0200

    Update fonctionUser.php

--- fonctionUser.php
+++ fonctionUser.php
@@ -167,18 +167,19 @@
 	}
 
 	function getNetworkInformation(){
-		var networkState = navigator.connection.type;
-	
-		var states = {};
-		states[Connection.UNKNOWN]  = 'Unknown connection';
-		states[Connection.ETHERNET] = 'Ethernet connection';
-		states[Connection.WIFI]     = 'WiFi connection';
-		states[Connection.CELL_2G]  = 'Cell 2G connection';
-		states[Connection.CELL_3G]  = 'Cell 3G connection';
-		states[Connection.CELL_4G]  = 'Cell 4G connection';
-		states[Connection.CELL]     = 'Cell generic connection';
-		states[Connection.NONE]     = 'No network connection';
-
+		$networkInformation = '<script>
+			var networkState = navigator.connection.type;
+		
+			var states = {};
+			states[Connection.UNKNOWN]  = "Unknown connection";
+			states[Connection.ETHERNET] = "Ethernet connection";
+			states[Connection.WIFI]     = "WiFi connection";
+			states[Connection.CELL_2G]  = "Cell 2G connection";
+			states[Connection.CELL_3G]  = "Cell 3G connection";
+			states[Connection.CELL_4G]  = "Cell 4G connection";
+			states[Connection.CELL]     = "Cell generic connection";
+			states[Connection.NONE]     = "No network connection";
+		</script>';
 		return states;
 	}
 ?>
