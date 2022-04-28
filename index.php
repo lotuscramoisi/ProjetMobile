012fd70eb680eaa5a13650e8ae271f5c2494d727
lotuscramoisi <filoodu412@gmail.com>
2022-04-29 00:54:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="eu">
+<html lang="fr">
 
 <head>
     <?php
@@ -8,7 +8,7 @@
     include 'connexiondb.php';
     include 'fonctionUser.php';
     // Connexion à la BDD en tant qu'admin
-    //$conn = connectDBasAdmin();
+    $conn = connectDBasAdmin();
     // Démarrage de la session pour créer les variables $_SESSION
     session_start();
 
