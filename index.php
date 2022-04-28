a02f984d3e8fe74f96328d0a61920854fca21b74
ddruart19 <dorian.druart@gmail.com>
2022-04-28 22:14:07 +0200

    pushragequit

--- index.php
+++ index.php
@@ -335,7 +335,7 @@
 <body class="d-flex flex-column h-100">
     <!-- Nav Bar -->
     <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <a class="navbar-brand" href="index.php">WhatsmyinfoToast</a>
+        <a class="navbar-brand" href="index.php">Whatsmyinfo</a>
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
 
         </ul>
