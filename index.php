de93441e6964fb79d0532ea5aa98e19e1e114b3c
ddruart19 <dorian.druart@gmail.com>
2022-04-28 20:31:40 +0200

    test

--- index.php
+++ index.php
@@ -107,7 +107,7 @@
                         $inputs.prop("disabled", false);
                     });
             });
-            
+
 
 
             // FIN AJAX FORMULAIRE LOGIN
@@ -185,41 +185,55 @@
 
             //DEBUT : ENREGISTREMENT DES DONNEES DES USERS DANS LA DB
             //Si l'utilisateur est connecté
-// <?php
-//             if (isset($_SESSION['login'])) {
+            // <?php
+                //             if (isset($_SESSION['login'])) {
 
-//                 echo "$.ajax({";
-//                 echo  "url: \"saveinfo.php\", //the page containing php script";
-//                 echo  "type: \"post\", //request type,";
-//                 echo  "dataType: 'json',";
-//                 echo  "data: {";
-//                 echo  "username:  \"" . $_SESSION['login'] ."\"";
-//                 echo  "},";
-//                 echo  "success: function(result) {}";
-//                 echo  "});";          
-//             }
-// ?>
+                //                 echo "$.ajax({";
+                //                 echo  "url: \"saveinfo.php\", //the page containing php script";
+                //                 echo  "type: \"post\", //request type,";
+                //                 echo  "dataType: 'json',";
+                //                 echo  "data: {";
+                //                 echo  "username:  \"" . $_SESSION['login'] ."\"";
+                //                 echo  "},";
+                //                 echo  "success: function(result) {}";
+                //                 echo  "});";          
+                //             }
+                // 
+                ?>
             //FIN   : ENREGISTREMENT DES DONNEES DES USERS DANS LA DB
 
             //DEBUT : ENREGISTREMENT DES DONNEES DES USERS DANS LA DB
             //Si l'utilisateur est connecté
-             <?php
-                if (isset($_SESSION['login'])) {
+            <?php
+            // if (isset($_SESSION['login'])) {
 
-                    echo "$.ajax({
-                        url: \"saveinfo.php\", //the page containing php script
-                        type: \"post\", //request type,
-                        dataType: 'json',
-                        data: {
-                            username:  \"" . $_SESSION['login'] .
-                        "\"},
-                        success: function(result) {
+            //     echo "$.ajax({
+            //             url: \"saveinfo.php\", //the page containing php script
+            //             type: \"post\", //request type,
+            //             dataType: 'json',
+            //             data: {
+            //                 username:  \"" . $_SESSION['login'] .
+            //         "\"},
+            //             success: function(result) {
 
-                            }   
-                        });";
-                }
-                
-                ?>
+            //                 }   
+            //             });";
+            // }
+
+            ?>
+
+            var isLogged = <?php echo ($_SESSION['login'] ? 'true' : 'false'); ?>;
+            if (isLogged == 'true') {
+                $.ajax({
+                    url: "saveinfo.php",
+                    type: "post",
+                    dataType: 'json',
+                    data: {
+                        username: <?php echo $_SESSION['login']; ?>
+                    },
+                    success: function(result) {}
+                });
+            }
             //FIN   : ENREGISTREMENT DES DONNEES DES USERS DANS LA DB
 
         });
