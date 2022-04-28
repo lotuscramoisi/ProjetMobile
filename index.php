f02229a90d1a8620730179581e39f7cac0fc8a18
lotuscramoisi <filoodu412@gmail.com>
2022-04-29 01:30:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -190,8 +190,6 @@
                 ?>
             //FIN   : ENREGISTREMENT DES DONNEES DES USERS DANS LA DB
 
-           
-
         });
         //Fin du document ready
 
@@ -199,18 +197,28 @@
         //Fonction qui regarde si les droits du navigateur permettent la géolocalisation
         function checkGeolocalisation(){
                 navigator.permissions.query({name: 'geolocation'}).then(function(permissionStatus) {
-                    document.getElementById(PermissionGranted).innerHTML = permissionStatus.state ;
+                    
                     if (permissionStatus.state === 'granted') {
                         $("#flexSwitchCheckDefault").prop("checked", true);
+                        document.getElementById("PermissionGranted").innerHTML = "Autorisé";
+                    }else if(permissionStatus.state === 'prompt') {
+                        $("#flexSwitchCheckDefault").prop("checked", false);
+                        document.getElementById("PermissionGranted").innerHTML = "En cours de demande";
+                    }else{
+                        $("#flexSwitchCheckDefault").prop("checked", false);
+                        document.getElementById("PermissionGranted").innerHTML = "Non autorisé";
                     }
 
                     permissionStatus.onchange = function() {
                         if (permissionStatus.state === 'granted') {
                             $("#flexSwitchCheckDefault").prop("checked", true);
-                        } else if (permissionStatus.state === 'prompt') {
-                            console.log("Prompted ?");
+                            document.getElementById("PermissionGranted").innerHTML = "Autorisé";
+                        }else if(permissionStatus.state === 'prompt') {
+                            $("#flexSwitchCheckDefault").prop("checked", false);
+                            document.getElementById("PermissionGranted").innerHTML = "En cours de demande";
                         }else{
                             $("#flexSwitchCheckDefault").prop("checked", false);
+                            document.getElementById("PermissionGranted").innerHTML = "Non autorisé";
                         }
                     };
                 });
