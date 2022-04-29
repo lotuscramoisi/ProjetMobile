ce2e2eebbb24cb57ba8c901ab484b8f976ab6111
lotuscramoisi <63124539+lotuscramoisi@users.noreply.github.com>
2022-04-29 09:14:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -344,7 +344,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>What's my info ?! wanna jump right now</title>
+    <title>What's my info ?!</title>
 </head>
 
 <body class="d-flex flex-column h-100">
