a907b8cfd4129c937521a9ce7b8a6542bbf1e673
ddruart19 <dorian.druart@gmail.com>
2022-04-30 11:07:34 +0200

    toast

--- index.php
+++ index.php
@@ -375,7 +375,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>What's my info ?!</title>
+    <title>What's my info ?!Toast</title>
 </head>
 
 <body class="d-flex flex-column h-100">
