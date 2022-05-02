14ad792198270fe4aedf5469bcd2ad1cec62d9c7
lotuscramoisi <63124539+lotuscramoisi@users.noreply.github.com>
2022-05-02 13:07:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -388,11 +388,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
     <a class="navbar-brand" href="index.php">Whatsmyinfo</a>
-
-     
-        <ul class="navbar-nav me-auto mb-2 mb-md-0">
-
-        </ul>
+        <ul class="navbar-nav me-auto mb-2 mb-md-0"></ul>
     <div>   
         <form class="form-inline d-flex">
             <!-- Button trigger modal -->
