d609bf26b8ae3700a32a4682b5c5a8ca6056f58a
ddruart19 <70936236+ddruart19@users.noreply.github.com>
2022-05-04 12:28:20 +0200

    test mail

--- index.php
+++ index.php
@@ -255,6 +255,9 @@
             if (email == '') {
                 $("#alertMessageRegister").append("Please enter email");
             }
+            else if(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
+                $("#alertMessageRegister").append("Invalide email");
+            }
             // If username not entered
             if (username == '') {
                 $("#alertMessageRegister").append("Please enter username");
