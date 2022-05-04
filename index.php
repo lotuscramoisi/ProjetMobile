6aff4002b0a7ccd55f66f257627fba9b1fb83a68
LionThibaut <thibaut.lion@gmail.com>
2022-05-04 10:47:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -610,7 +610,7 @@
                                 </li>
 								<li class="list-group-item d-flex justify-content-between align-items-center">
                                     Degrés d'orientation de l'écran
-                                    <span class="badge badge-primary badge-pill" id="DegOrient"></span>
+                                    <span class="badge badge-primary badge-pill" id="DegOrient"><?php echo getDegOrient() ?></span>
                                 </li>
                                 <li class="list-group-item d-flex justify-content-between align-items-center">
                                     Permission
