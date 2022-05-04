0417294263e42fe2943a194bfae787d0b3a526da
LionThibaut <thibaut.lion@gmail.com>
2022-05-04 09:58:08 +0200

    Update fonctionUser.php

--- fonctionUser.php
+++ fonctionUser.php
@@ -276,6 +276,7 @@
 	
 	function getOrient(){
 		$orient = '<script>
+					document.getElementById("Orient").innerHTML = screen.orientation.type;
 					screen.orientation.onchange = function(e) {
 						document.getElementById("Orient").innerHTML = screen.orientation.type;
 					}
