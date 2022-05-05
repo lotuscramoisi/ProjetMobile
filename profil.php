d300849f9d2d852e31aa71b561d37cd412b245dc
lotuscramoisi <filoodu412@gmail.com>
2022-05-05 17:37:03 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -114,13 +114,13 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="index.php">Whatsmyinfo</a>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navBareExpendId"
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navBareExpendIdProfil"
             aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
 
-        <div class="collapse navbar-collapse" id="navBareExpendId">
+        <div class="collapse navbar-collapse" id="navBareExpendIdProfil">
             <ul class="navbar-nav me-auto mb-2 mb-md-0"></ul>
             <!-- Button trigger modal -->
             <?php
