bddf3a9ec111f9d3cd82a6ab03ee1b15315fd2ba
lotuscramoisi <filoodu412@gmail.com>
2022-05-05 17:40:06 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -16,146 +16,156 @@
 
     ?>
     <!-- DEBUT IMPORT -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"
+        integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous">
+    </script>
     <!-- Test -->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"
+        integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="cookiealert.css">
     <link rel="stylesheet" href="style.css">
     <!-- FIN IMPORT -->
     <script>
-        $(document).ready(function() {
-            // Récupération de la liste des utilisateurs + affichage de celle ci
+    $(document).ready(function() {
+        // Récupération de la liste des utilisateurs + affichage de celle ci
+        $.ajax({
+            url: "getUserList.php", //the page containing php script
+            async: false,
+            type: "post", //request type,
+            dataType: 'json',
+            data: {},
+            success: function(result) {
+
+                //Affichage de chaque ligne des utilisateurs récupérés
+                for (var r of result) {
+                    //Affichage d'un user admin
+                    if (r.ISADMIN == 1) {
+                        $('<tr></tr>').html(
+                            '<td>' + r.USERNAME + '</td>' +
+                            '<td>' + r.USERMAIL + '</td>' +
+                            '<td>' + r.SIGNUPDATE + '</td>' +
+                            '<td> <div class="form-check form-switch ml-5">' +
+                            '<input class="form-check-input genial" type="checkbox" role="switch" id="flexSwitchCheckDefault" checked>' +
+                            '</div></td>' +
+                            '<td><button type="button" class="btn btn-danger">Delete</button>' +
+                            '</td>'
+                        ).appendTo('#usertable');
+                    }
+                    //Affichage d'un user non admin
+                    else {
+                        $('<tr></tr>').html(
+                            '<td>' + r.USERNAME + '</td>' +
+                            '<td>' + r.USERMAIL + '</td>' +
+                            '<td>' + r.SIGNUPDATE + '</td>' +
+                            '<td> <div class="form-check form-switch ml-5">' +
+                            '<input class="form-check-input genial" type="checkbox" role="switch" id="flexSwitchCheckDefault">' +
+                            '</div></td>' +
+                            '<td><button type="button" class="btn btn-danger">Delete</button>' +
+                            '</td>'
+                        ).appendTo('#usertable');
+                    }
+                }
+            }
+        });
+
+
+        //Changement du droit d'administrateur quand click sur le switch
+        $(".form-check-input").click(function(event) {
             $.ajax({
-                url: "getUserList.php", //the page containing php script
+                url: "setAdmin.php", //the page containing php script
                 async: false,
                 type: "post", //request type,
                 dataType: 'json',
-                data: {},
-                success: function(result) {
-
-                    //Affichage de chaque ligne des utilisateurs récupérés
-                    for (var r of result) {
-                        //Affichage d'un user admin
-                        if (r.ISADMIN == 1) {
-                            $('<tr></tr>').html(
-                                '<td>' + r.USERNAME + '</td>' +
-                                '<td>' + r.USERMAIL + '</td>' +
-                                '<td>' + r.SIGNUPDATE + '</td>' +
-                                '<td> <div class="form-check form-switch ml-5">' +
-                                '<input class="form-check-input genial" type="checkbox" role="switch" id="flexSwitchCheckDefault" checked>' +
-                                '</div></td>' +
-                                '<td><button type="button" class="btn btn-danger">Delete</button>' +
-                                '</td>'
-                            ).appendTo('#usertable');
-                        }
-                        //Affichage d'un user non admin
-                        else {
-                            $('<tr></tr>').html(
-                                '<td>' + r.USERNAME + '</td>' +
-                                '<td>' + r.USERMAIL + '</td>' +
-                                '<td>' + r.SIGNUPDATE + '</td>' +
-                                '<td> <div class="form-check form-switch ml-5">' +
-                                '<input class="form-check-input genial" type="checkbox" role="switch" id="flexSwitchCheckDefault">' +
-                                '</div></td>' +
-                                '<td><button type="button" class="btn btn-danger">Delete</button>' +
-                                '</td>'
-                            ).appendTo('#usertable');
-                        }
-                    }
-                }
-            });
-
-
-            //Changement du droit d'administrateur quand click sur le switch
-            $(".form-check-input").click(function(event) {
-                $.ajax({
-                    url: "setAdmin.php", //the page containing php script
-                    async: false,
-                    type: "post", //request type,
-                    dataType: 'json',
-                    data: {
-                        username: $(this).parent().parent().siblings().html(),
-                        permission: $(this).is(':checked')
-                    },
-                    success: function(result) {}
-                });
-            });
-
-            //Suppression d'un utilisateur
-            $(".btn-danger").click(function(event) {
-                $.ajax({
-                    url: "deleteUser.php", //the page containing php script
-                    async: false,
-                    type: "post", //request type,
-                    dataType: 'json',
-                    data: {
-                        username: $(this).parent().siblings().html()
-                    },
-                    success: function(result) {}
-                });
-                $(this).parent().parent().remove();
-            });
-            //Debut récupération des stats
-            $.ajax({
-                    url: "getStats.php", //the page containing php script
-                    async: false,
-                    type: "post", //request type,
-                    dataType: 'json',
-                    data: {},
-                    success: function(result) {
-                        var donnees = JSON.parse(result);
-                        firstData = result.Total;
-                    }
-                });
-            //Fin récupération des stats
-        });
-        //Fin du document ready
-
-
-        //DEBUT FONCTION POUR LES GRAPHIQUES PIECHART
-        window.onload = function() {
-
-            var chart = new CanvasJS.Chart("chartContainer", {
-                animationEnabled: true,
-                title: {
-                    text: "OS utilisation by users"
+                data: {
+                    username: $(this).parent().parent().siblings().html(),
+                    permission: $(this).is(':checked')
                 },
-                data: [{
-                    type: "pie",
-                    startAngle: 240,
-                    yValueFormatString: "##0.00\"%\"",
-                    indexLabel: "{label} {y}",
-                    dataPoints: [{
-                            y: firstData,
-                            label: "Windows"
-                        },
-                        {
-                            y: 7.31,
-                            label: "Bing"
-                        },
-                        {
-                            y: 7.06,
-                            label: "Baidu"
-                        },
-                        {
-                            y: 4.91,
-                            label: "Yahoo"
-                        },
-                        {
-                            y: 1.26,
-                            label: "Others"
-                        }
-                    ]
-                }]
+                success: function(result) {}
             });
-            chart.render();
+        });
 
-        }
-        //FIN FONCTION POUR LES GRAPHIQUES PIECHART
+        //Suppression d'un utilisateur
+        $(".btn-danger").click(function(event) {
+            $.ajax({
+                url: "deleteUser.php", //the page containing php script
+                async: false,
+                type: "post", //request type,
+                dataType: 'json',
+                data: {
+                    username: $(this).parent().siblings().html()
+                },
+                success: function(result) {}
+            });
+            $(this).parent().parent().remove();
+        });
+        //Debut récupération des stats
+        $.ajax({
+            url: "getStats.php", //the page containing php script
+            async: false,
+            type: "post", //request type,
+            dataType: 'json',
+            data: {},
+            success: function(result) {
+                var donnees = JSON.parse(result);
+                firstData = result.Total;
+            }
+        });
+        //Fin récupération des stats
+    });
+    //Fin du document ready
+
+
+    //DEBUT FONCTION POUR LES GRAPHIQUES PIECHART
+    window.onload = function() {
+
+        var chart = new CanvasJS.Chart("chartContainer", {
+            animationEnabled: true,
+            title: {
+                text: "OS utilisation by users"
+            },
+            data: [{
+                type: "pie",
+                startAngle: 240,
+                yValueFormatString: "##0.00\"%\"",
+                indexLabel: "{label} {y}",
+                dataPoints: [{
+                        y: firstData,
+                        label: "Windows"
+                    },
+                    {
+                        y: 7.31,
+                        label: "Bing"
+                    },
+                    {
+                        y: 7.06,
+                        label: "Baidu"
+                    },
+                    {
+                        y: 4.91,
+                        label: "Yahoo"
+                    },
+                    {
+                        y: 1.26,
+                        label: "Others"
+                    }
+                ]
+            }]
+        });
+        chart.render();
+
+    }
+    //FIN FONCTION POUR LES GRAPHIQUES PIECHART
     </script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -165,9 +175,17 @@
 
 <body>
     <!-- Nav Bar -->
-    <nav class="navbar navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="index.php">Whatsmyinfo</a>
-        <form class="form-inline">
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navBareExpendIdAdmin"
+            aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+
+        <div class="collapse navbar-collapse" id="navBareExpendIdAdmin">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0"></ul>
             <!-- Button trigger modal -->
             <?php
             if (isset($_SESSION['login'])) {
@@ -182,7 +200,7 @@
                 echo '</button></a>';
             }
             ?>
-        </form>
+            <div>
     </nav>
 
     <table class="table table-striped">
@@ -201,7 +219,8 @@
 
     <!-- START Bootstrap-Cookie-Alert -->
     <div class="alert text-center cookiealert" role="alert">
-        <b>Do you like cookies?</b> &#x1F36A; We use cookies to ensure you get the best experience on our website. <a href="https://cookiesandyou.com/" target="_blank">Learn more</a>
+        <b>Do you like cookies?</b> &#x1F36A; We use cookies to ensure you get the best experience on our website. <a
+            href="https://cookiesandyou.com/" target="_blank">Learn more</a>
 
         <button type="button" class="btn btn-primary btn-sm acceptcookies">
             I agree
