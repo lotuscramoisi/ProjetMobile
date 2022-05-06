6a154721ee309e8cd19e62f090f88a78710e9af1
LionThibaut <thibaut.lion@gmail.com>
2022-05-06 15:46:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -607,15 +607,15 @@
                                     <span class="badge badge-primary badge-pill" id="GyroZ"><?php echo getGyroZ() ?></span>
                                 </li>
 								<li class="list-group-item d-flex justify-content-between align-items-center">
-                                    Accéléromètre Bêta (β)
+                                    Accéléromètre X
                                     <span class="badge badge-primary badge-pill" id="AccelX"><?php echo getAccelX() ?></span>
                                 </li>
 								<li class="list-group-item d-flex justify-content-between align-items-center">
-                                    Accéléromètre Gamma (ɣ)
+                                    Accéléromètre Y
                                     <span class="badge badge-primary badge-pill" id="AccelY"><?php echo getAccelY() ?></span>
                                 </li>
 								<li class="list-group-item d-flex justify-content-between align-items-center">
-                                    Accéléromètre Alpha (α)
+                                    Accéléromètre Z
                                     <span class="badge badge-primary badge-pill" id="AccelZ"><?php echo getAccelZ() ?></span>
                                 </li>
                             </div>
