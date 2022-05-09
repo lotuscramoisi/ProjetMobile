a1cb5bed5358e8ff748eef2381b6968b6fadef58
ddruart19 <dorian.druart@gmail.com>
2022-05-09 20:38:22 +0200

    toast

--- getStats.php
+++ getStats.php
@@ -10,7 +10,7 @@ if (isset($_SESSION["admin"])) {
 
         //DEBUT RECHERCHE DES SESSIONS
         // Requête sql de recherche des sessions liées à l'username dans la DB
-        $sql = "select count(*) as Total from SESSIONUSER where USEROS like 'Windows'";
+        $sql = "select count(*) as y, USEROS as label from SESSIONUSER where USEROS like 'Windows'";
 
         // Préparation de la requête
         $stmt = $conn->prepare($sql);
